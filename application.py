e733307617b9ec6ccbcb6a3cb4ae045b2af283cb
Robby Renz <nxgrenz@gmail.com>
2019-09-25 17:39:42 -0500

    fix bug of not showing user has no notes

--- application.py
+++ application.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+notes = []
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    note = request.form.get("text")
+    notes.append(note)
+    return render_template("index.html", notes=notes)
